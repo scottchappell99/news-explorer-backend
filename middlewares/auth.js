e651439b38119e34bcee9c78e29e933f3d07c147
scott.chappell <scott.chappell99@gmail.com>
2025-02-19 14:01:03 -0500

    Add authorization middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const { UnauthorizedError } = require("../utils/errors/UnauthorizedError");
+const { JWT_SECRET } = require("../utils/config");
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    next(new UnauthorizedError("Authorization Required"));
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    next(new UnauthorizedError("Authorization Required"));
+  }
+
+  req.user = payload;
+
+  return next();
+};
+
+module.exports = { auth };
